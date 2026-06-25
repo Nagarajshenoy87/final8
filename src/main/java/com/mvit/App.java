c0259b55be576ba85742adcae20f72e569d727ad
Nagaraj <nagarajshenoy20@gmail.com>
2026-06-25 07:00:57 +0530

    created maven

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,13 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is program 8 CI and CD" );
+    }
+}
